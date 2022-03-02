0bc7caa025981fe7d91c423a68befc960deabbe6
René Kijewski <Kijewski@users.noreply.github.com>
2022-03-02 22:41:37 +0100

    Include pre-1970 data, too (#18)
    C.f. https://www.theregister.com/2021/09/28/time_zone_database_controversy/

--- tzdb/src/generated.rs
+++ tzdb/src/generated.rs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8f7ab1091f4e7f34379a9fce3dbfe0305161b15105f7f08dcccf38b72ce53243
-size 1400966
+oid sha256:122faf74e5db597b47f14ffa28a935e7e3b02fbf39a738ffbdcf78711b1dd317
+size 1593306
