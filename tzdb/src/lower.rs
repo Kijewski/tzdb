08ee3c0b5a3b0507085962ae2cd3e20c6fe59062
René Kijewski <Kijewski@users.noreply.github.com>
2022-06-29 23:24:07 +0200

    Fix error if build and target platform have different pointer widths (#58)
    Fix error for div. build/target platform ptr width

--- tzdb/src/lower.rs
+++ tzdb/src/lower.rs
@@ -1,19 +1,23 @@
 use byte_slice_cast::AsMutByteSlice;
 use phf_shared::{FmtConst, PhfBorrow, PhfHash};
 
-pub(crate) type Slice = [usize; ((32 * 8) / usize::BITS) as usize];
+#[allow(unused)]
+pub(crate) const FULL_TO_LOWER_MAX_LEN: usize = 31;
+
+pub(crate) type Slice = [u64; 4];
 
 #[derive(PartialEq, Eq, Hash)]
 pub(crate) struct Lower(pub(crate) Slice);
 
 #[inline]
 pub(crate) fn full_to_lower(s: &[u8]) -> Lower {
-    // The caller ensures that s.len() <= 32.
+    // The caller ensures that s.len() <= FULL_TO_LOWER_MAX_LEN.
     let mut lower = Slice::default();
     lower.as_mut_byte_slice()[..s.len()].copy_from_slice(s);
     for c in &mut lower {
-        *c |= 0x2020_2020_2020_2020_2020_2020_2020_2020_u128 as usize;
+        *c |= 0x2020_2020_2020_2020;
     }
+    lower.as_mut_byte_slice()[s.len()] = 0;
     Lower(lower)
 }
 
@@ -21,28 +25,7 @@ impl PhfHash for Lower {
     #[inline]
     fn phf_hash<H: core::hash::Hasher>(&self, state: &mut H) {
         for c in self.0 {
-            #[cfg(target_pointer_width = "8")]
-            (c as u8).phf_hash(state);
-            #[cfg(target_pointer_width = "16")]
-            (c as u16).phf_hash(state);
-            #[cfg(target_pointer_width = "32")]
-            (c as u32).phf_hash(state);
-            #[cfg(target_pointer_width = "64")]
-            (c as u64).phf_hash(state);
-            #[cfg(target_pointer_width = "128")]
-            (c as u128).phf_hash(state);
-
-            #[cfg(not(any(
-                target_pointer_width = "8",
-                target_pointer_width = "16",
-                target_pointer_width = "32",
-                target_pointer_width = "64",
-                target_pointer_width = "128",
-            )))]
-            compile_error!(
-                "Unsupported target_pointer_width. \
-                Please file a bug report to <https://github.com/Kijewski/tzdb/>."
-            );
+            c.phf_hash(state);
         }
     }
 }
