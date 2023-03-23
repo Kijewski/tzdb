5e6ccb1f3b412b2f72c503fd7ea9bdf5219b2642
René Kijewski <rene.kijewski@fu-berlin.de>
2023-03-24 00:29:17 +0100

    Update to 2023a, simplify features, re-use v0.5.x
    * Update to [Time Zone Database 2023a](https://mm.icann.org/pipermail/tz-announce/2023-January/000077.html)
    * Make the features `binary`, `by-name`, `list` always present
    * Use the same generated code as the v0.5.x branch

--- test-artichoke/src/main.rs
+++
@@ -1,8 +0,0 @@
-use spinoso_time::tzrs::Time;
-
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let now = Time::now()?;
-    println!("now = {}", &now);
-    println!("now? = {:#?}", &now);
-    Ok(())
-}
