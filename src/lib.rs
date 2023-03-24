a9fe54f8ba37bdb7e98971a7d33229cd16d52a65
René Kijewski <rene.kijewski@fu-berlin.de>
2023-03-24 01:27:28 +0100

    Fix typos

--- src/lib.rs
+++ src/lib.rs
@@ -43,7 +43,7 @@
 //! Static time zone information for [tz-rs](https://crates.io/crates/tz-rs).
 //!
 //! This crate provides all time zones found in the [Time Zone Database](https://www.iana.org/time-zones),
-//! currently in the version 2023a (released 2023-02-22).
+//! currently in the version 2023a (released 2023-03-22).
 //!
 //! See the documentation for a full list the the contained time zones:
 //! <https://docs.rs/tzdb/latest/tzdb/time_zone/index.html>
