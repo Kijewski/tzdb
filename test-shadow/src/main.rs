a7337b01b865c119c644b1db27e689ed97d292d9
René Kijewski <rene.kijewski@fu-berlin.de>
2023-08-10 19:36:15 +0200

    CI: remove test-artichoke and test-shadow
    Semver breaking changes a proper version number.
    (I wasn't updating the tests anyway.)

--- test-shadow/src/main.rs
+++
@@ -1,9 +0,0 @@
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    shadow_rs::shadow!(build);
-
-    println!("{}", build::BUILD_TIME);
-    println!("{}", build::BUILD_TIME_2822);
-    println!("{}", build::BUILD_TIME_3339);
-
-    Ok(())
-}
