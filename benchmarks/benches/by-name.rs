19cf740192264411ae6eb4e84e462ae465d400a6
René Kijewski <rene.kijewski@fu-berlin.de>
2024-02-01 04:34:07 +0100

    Simplify benchmark

--- benchmarks/benches/by-name.rs
+++ benchmarks/benches/by-name.rs
@@ -1,19 +1,90 @@
-use std::convert::TryInto;
+use std::iter::FusedIterator;
 use std::str::FromStr;
-use std::time::{Duration, Instant};
+use std::time::Duration;
 
 use criterion::black_box;
+use minstant::Instant;
 use rand::seq::{IteratorRandom, SliceRandom};
 use rand::SeedableRng;
 use rand_xoshiro::Xoroshiro128PlusPlus;
 use tzdb_data::{find_raw, TZ_NAMES};
 
+fn main() {
+    #[cfg(not(miri))]
+    {
+        criterion::criterion_group!(benches, benchmark_by_name);
+        benches();
+
+        criterion::Criterion::default()
+            .configure_from_args()
+            .with_plots()
+            .final_summary();
+    }
+}
+
 fn benchmark_by_name(c: &mut criterion::Criterion) {
+    let names = generate_names();
+    let names: Vec<&str> = names.iter().map(String::as_str).collect();
+
+    macro_rules! bench_functions {
+        ($($ident:ident($name:pat) $body:block)*) => { $({
+            fn $ident(limit: u64, names: &[&str]) -> Duration {
+                let start = Instant::now();
+                for $name in names.iter().cycle().take64(limit) {
+                    let _ = black_box($body);
+                }
+                start.elapsed()
+            }
+
+            c.bench_function(
+                stringify!($id),
+                |b| b.iter_custom(|iters| $ident(iters, black_box(&names))),
+            );
+        })* };
+    }
+
+    bench_functions! {
+        tzdb_find_raw(name) {
+            crate::find_raw(name.as_bytes())
+        }
+        chrono_tz_from_str(name) {
+            chrono_tz::Tz::from_str(name)
+        }
+        chrono_tz_from_str_insensitive(name) {
+            chrono_tz::Tz::from_str_insensitive(name)
+        }
+    }
+}
+
+fn generate_names() -> Vec<String> {
+    let rng = &mut Xoroshiro128PlusPlus::seed_from_u64(2024_02_01);
+
+    let mut names: Vec<_> = TZ_NAMES.iter().map(|s| String::from(*s)).collect();
+
+    // insert 10% unknown names
+    for _ in 0..names.len().div_ceil(10) {
+        let mut continent = *b"abcdefghijklmnopqrstuvwxyz";
+        let mut city = *b"abcdefghijklmnopqrstuvwxyz";
+
+        continent.shuffle(rng);
+        city.shuffle(rng);
+
+        let continent = &mut continent[..(4..=8).choose(rng).unwrap()];
+        let city = &mut city[..(4..=12).choose(rng).unwrap()];
+
+        continent[0] = continent[0].to_ascii_uppercase();
+        city[0] = city[0].to_ascii_uppercase();
+
+        let continent = std::str::from_utf8(continent).unwrap();
+        let city = std::str::from_utf8(city).unwrap();
+
+        names.push(format!("{}/{}", continent, city));
+    }
+
     // collect all names with "random" capitalization
-    let mut names: Vec<(String, usize)> = TZ_NAMES
-        .iter()
-        .flat_map(|&name| {
-            let raw_len = find_raw(name.as_bytes()).unwrap().len();
+    let mut names: Vec<_> = names
+        .into_iter()
+        .flat_map(|name| {
             let upper = name.to_uppercase();
             let lower = name.to_lowercase();
             let inverted = name
@@ -46,111 +117,36 @@ fn benchmark_by_name(c: &mut criterion::Criterion) {
                     }
                 })
                 .collect();
-            [
-                (name.to_owned(), raw_len),
-                (upper, raw_len),
-                (lower, raw_len),
-                (inverted, raw_len),
-                (spongebob1, raw_len),
-                (spongebob2, raw_len),
-            ]
+            [name, upper, lower, inverted, spongebob1, spongebob2]
         })
         .collect();
 
-    // insert a bunch of unknown names
-    for idx in 0..100 {
-        let mut rng = Xoroshiro128PlusPlus::seed_from_u64(idx);
-
-        let mut continent = *b"abcdefghijklmnopqrstuvwxyz";
-        let mut city = *b"abcdefghijklmnopqrstuvwxyz";
-
-        continent.shuffle(&mut rng);
-        city.shuffle(&mut rng);
-
-        let continent = &mut continent[..(4..=8).choose(&mut rng).unwrap()];
-        let city = &mut city[..(4..=12).choose(&mut rng).unwrap()];
-
-        continent[0] = continent[0].to_ascii_uppercase();
-        city[0] = city[0].to_ascii_uppercase();
-
-        let continent = std::str::from_utf8(continent).unwrap();
-        let city = std::str::from_utf8(city).unwrap();
-
-        let raw_name = format!("{}/{}", continent, city);
-        let raw_len = crate::find_raw(raw_name.as_bytes())
-            .unwrap_or_default()
-            .len();
-        names.push((raw_name, raw_len));
-    }
-
-    let orig_names: Vec<_> = names
-        .iter()
-        .map(|&(ref s, len)| (s.as_str(), len))
-        .collect();
-    let mut names = Vec::with_capacity(orig_names.len());
-
-    // benchmark per name lookup time
-
-    macro_rules! bench_function {
-        ($id:literal($name:pat, $raw_len:pat) $body:expr) => {
-            c.bench_function($id, |b| {
-                b.iter_custom(|iters| {
-                    let mut nanos = 0;
-                    for i in 0..iters {
-                        names.clear();
-                        names.extend_from_slice(orig_names.as_slice());
-                        names.shuffle(&mut Xoroshiro128PlusPlus::seed_from_u64(i));
-                        let names = black_box(names.as_slice());
-
-                        let start = Instant::now();
-                        for &($name, $raw_len) in names {
-                            $body
-                        }
-                        nanos += start.elapsed().as_nanos();
-                    }
-                    Duration::from_nanos(
-                        (nanos / orig_names.len() as u128)
-                            .try_into()
-                            .expect("Did the test take 584 years to finish?"),
-                    )
-                });
-            });
-        };
-    }
-
-    bench_function!(
-        "tzdb::find_raw"
-        (name, raw_len)
-        assert_eq!(
-            raw_len,
-            black_box(crate::find_raw(name.as_bytes())).unwrap_or_default().len(),
-        )
-    );
-    bench_function!(
-        "chrono_tz::Tz::from_str"
-        (name, _)
-        if let Ok(tz) = chrono_tz::Tz::from_str(name) {
-            assert!(!black_box(tz.name()).is_empty());
-        }
-    );
-    bench_function!(
-        "chrono_tz::Tz::from_str_insensitive"
-        (name, _)
-        if let Ok(tz) = chrono_tz::Tz::from_str_insensitive(name) {
-            assert!(!black_box(tz.name()).is_empty());
-        }
-    );
+    // randomize order
+    names.shuffle(rng);
+    names
 }
 
-fn main() {
-    #[cfg(not(miri))]
-    {
-        criterion::criterion_group!(benches, benchmark_by_name);
-        benches();
+#[derive(Debug, Clone, Copy)]
+struct Take64<I> {
+    iter: I,
+    limit: u64,
+}
 
-        criterion::Criterion::default()
-            .configure_from_args()
-            .with_plots()
-            .final_summary();
+impl<I: Iterator> Iterator for Take64<I> {
+    type Item = I::Item;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.limit = self.limit.checked_sub(1)?;
+        self.iter.next()
     }
 }
+
+impl<I: FusedIterator> FusedIterator for Take64<I> {}
+
+trait Take64Ext: Sized {
+    fn take64(self, limit: u64) -> Take64<Self> {
+        Take64 { iter: self, limit }
+    }
+}
+
+impl<I: Iterator> Take64Ext for I {}
