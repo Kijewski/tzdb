02cb82ae0eaa7129de6ce5bb19e2412733a23be0
René Kijewski <rene.kijewski@fu-berlin.de>
2026-03-15 21:09:45 +0100

    Update to 2026a

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 .DELETE_ON_ERROR:
 
-TZDB_VERSION := tzdb-2025c
+TZDB_VERSION := tzdb-2026a
 
 tzdb_data/src/generated/mod.rs: tmp/${TZDB_VERSION}/usr/share/zoneinfo/ tzdb.tar.lz.sha
 	cd make-tzdb && cargo r -- ../$(@D) ../$< ../tzdb.tar.lz.sha
--- make-tzdb/src/main.rs
+++ make-tzdb/src/main.rs
@@ -148,21 +148,8 @@ fn gen_mod(args: &mut impl Iterator<Item = String>, target_dir: &Path) -> Result
 
     let r = format!(
         r#"{GENERATED_FILE}
-// MIT No Attribution
-//
-// Copyright 2022-2024 René Kijewski <crates.io@k6i.de>
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
-// associated documentation files (the "Software"), to deal in the Software without restriction,
-// including without limitation the rights to use, copy, modify, merge, publish, distribute,
-// sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
-// furnished to do so.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
-// NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// SPDX-License-Identifier: MIT-0 OR MIT OR Apache-2.0
+// SPDX-FileCopyrightText: 2022-2026 René Kijewski <crates.io@k6i.de>
 
 #![allow(unknown_lints)]
 #![allow(clippy::pedantic)]
